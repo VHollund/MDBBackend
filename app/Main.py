d6a00a756b35c1d3370e8ff5cf3e0b90b16f87e7
Vetle Ørstavik Hollund <vetleh15@uia.no>
2020-12-04 11:56:49 +0100

    now with daily updates

--- app/Main.py
+++ app/Main.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api
 import pymongo
 from bson.json_util import dumps
 from app.TMDBNeo4j import Neo4jConn
-
+import requests as req
 
 
 app = Flask(__name__, template_folder='templates')
@@ -17,21 +17,32 @@ movies = mydb["movies"]
 credits = mydb["credits"]
 
 
-def update_votes():
+def update_mongo(new_data):
     new_data = request.get()
     for x in new_data:
-        movies.update({"id": x['id']}, {"$set": {"votes": f"{x.votes}"}})
+        movies.update({"id": x[0]}, {"$set": {"vote_count": f"{x[1][0]}", "vote_average": f"{x[0][1]}"}})
+        print(f"MongoDB: updated movie {x[0]}")
 
 
-def update_rating():
-    new_data=request.get()
+def update_neo4j(new_data):
+    neo4j = Neo4jConn("bolt://v-hollund.no:7687", "neo4j", "dbiola")
     for x in new_data:
-        movies.update({"id": x['id']}, {"$set": {"votes": f"{x.votes}"}})
+        results = neo4j.query("MATCH p=(m:Movie { movieID: toString(%i)}) SET m.voteAverage = toString(%i) "
+                              "SET m.voteCount=toString(%i) return p" % (x[0], x[1][1], x[1][0]))
+        print(f"Neo4j: updated movie {x[0]}")
 
+def update_data(new_data):
+    id=[]
+    new_data = {}
+    print("Update Starting")
+    for x in movies.find():
+        id.append(x['id'])
+    for x in id:
+        res = req.get(f"https://api.themoviedb.org/3/movie/{x}?api_key=0edacbafa803b562070aa4928160edbf")
+        new_data[res.json()['id']] = (res.json()['vote_count'], res.json()['vote_average'])
+    update_mongo(new_data)
+    update_neo4j(new_data)
 
-def update_data():
-    update_votes()
-    update_rating()
 
 
 @app.route('/')
